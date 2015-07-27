124acdb2d76b9cc00d349c7607787d43ba1ade5a
Ryan Whalen <rwhalen3@malwr.local>
2015-07-27 07:45:12 -0500

    Added BlueCoat Local Transform.

---
+++ bluecoat_category_lookup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+# Search BlueCoat SiteReview for URL/Domain and return the category
+#
+#
+
+from MaltegoTransform import *
+import sys
+import re
+import requests
+import simplejson
+
+input_url = sys.argv[1]
+m = MaltegoTransform()
+
+url = 'https://sitereview.bluecoat.com/rest/categorization'
+headers = {'X-Requested-With':'XMLHttpRequest', 
+           'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36', 
+           'Origin':'https://sitereview.bluecoat.com',
+           'Referer':'https://sitereview.bluecoat.com/sitereview.jsp'}
+
+payload = 'url=%s' % (input_url)
+r = requests.post(url,headers=headers,data=payload)
+response_dict = simplejson.loads(r.text)
+
+try:
+    categorization = response_dict.get("categorization", {})
+    ratedate = response_dict.get("ratedate", {})
+    unrated = response_dict.get("unrated", {})
+except:
+    m.returnOutput()
+
+if (unrated == 'true'):
+    category = 'Unrated'
+    m.addEntity('maltego.Phrase', category)
+    m.returnOutput()    
+else:
+    try:
+        match = re.match("^.+\>(.+)\<.+", categorization)
+        category = match.group(1)
+        m.addEntity('maltego.Phrase', category)
+        m.returnOutput()        
+    except:
+        m.returnOutput()
+
